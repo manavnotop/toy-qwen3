c658edd84f8beb622c521c55dcd42cb67fddde40
manavnotop <manuagarwal1443@gmail.com>
2025-08-01 19:57:38 +0530

    Add RoPE

--- main.py
+++ main.py
@@ -42,3 +42,45 @@ class RMSNormalisation(nn.Module):
 
     #cast back to original input data type
     return norm_x.to(input_type)
+  
+def compute_rope_params(head_dims, theta_base=10000, context_length=4096, dtype=torch.float32):
+  assert head_dims % 2 == 0, "Embedding dimension must be even"
+
+  #compute inverse frequencies (torch.arange gives values 0, 2, 4, ...head_dim - 2)
+  inv_freq = 1.0 / (theta_base ** (torch.arange(0, head_dims, 2, dtype=dtype) / head_dims)) # -> (head_dims/2,)
+
+  #generate position indexes
+  position = torch.arange(context_length, dtype=dtype) # -> (context_length)
+
+  #compute angles
+  #position[:, None] converts (context_length) -> (context_length, 1)
+  #inv_freq[None, :] converts (head_dims/2,) -> (1, head_dims/2)
+  angles = position[:, None] * inv_freq[None, :] # -> (context_length, head_dims/2)
+
+  #expand angles to match head_dim(duplicates angles)
+  angles = torch.cat(angles, angles, dim=1) # -> (context_length, head_dims)
+
+  #precompute sine and cosine
+  cos = torch.cos(angles)
+  sin = torch.cos(angles)
+
+  return cos, sin
+
+def apply_rope(x, cos, sin):
+  batch_size, num_heads, seq_len, head_dim = x.shape
+  assert head_dim % 2 == 0, "Head dimensions must be even"
+
+  #split input(x) into first half and second half
+  x1 = x[..., : head_dim // 2] # for head_dim 64 -> [ 1, ..., 32]
+  x2 = x[..., head_dim // 2 :] # for head_dim 64 -> [33, ..., 64]
+
+  #adjust shapes of cos and sin to match with x(batch_size, num_heads, seq_length, head_dim)
+  cos = cos[:seq_len, :].unsqueeze(0).unsqueeze(0) # (seq_length, head_dim) -> (1, 1, seq_length, head_dim)
+  sin = sin[:seq_len, :].unsqueeze(0).unsqueeze(0) 
+
+  #apply rotary transformation
+  rotated = torch.cat((-x2, x1), dim=-1)
+  x_rotated = (x * cos) + (rotated * sin)
+
+  return x_rotated.to(dtype=x.dtype)
+
